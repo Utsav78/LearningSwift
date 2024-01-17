9efdacdd5d821bead8b7f1baf45257d1defb0593
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-01-17 12:01:04 +1030

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Utsav Budathoki on 17/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
