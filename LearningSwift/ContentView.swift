0353be16bf3abd49563e3f6b3443a6ecd7983dc3
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-01-18 09:33:12 +1030

    alert implemented

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, World!")
+            Text("Hello Utsav!")
+                .onTapGesture {
+                    showAlert = true
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("Popup message"),
+                          message: Text("You clicked the text"),
+                          dismissButton: .default(Text("OK"))
+                          )
+                }
         }
         .padding()
     }
