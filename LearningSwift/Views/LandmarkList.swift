9e1ebb2df6b4764a9f5760e19b02777af5598253
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-02-07 12:51:17 +1030

    feat: Add Favorite only toggle

--- LearningSwift/Views/LandmarkList.swift
+++ LearningSwift/Views/LandmarkList.swift
@@ -17,18 +17,24 @@ struct LandmarkList: View {
     }
     
     var body: some View {
-//        List(landmarks, id: \.id) { landmark in
-//            LandmarkRow(landmark: landmark)
-//        }
-        // when we use Identifiable in Landmark, we can directly create the list of landmarks . no id required 
+        //        List(landmarks, id: \.id) { landmark in
+        //            LandmarkRow(landmark: landmark)
+        //        }
+        // when we use Identifiable in Landmark, we can directly create the list of landmarks . no id required
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly) {
+                    Text("Favorite Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text("Select a Landmark")
