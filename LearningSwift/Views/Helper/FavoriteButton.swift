aa729e3f12765a15d8aedd82c26241bf1e0a7cad
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-02-10 16:02:01 +1030

    feat: Toggle Favorite Landmark

---
+++ LearningSwift/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  LearningSwift
+//
+//  Created by Utsav Budathoki on 10/2/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star-fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
