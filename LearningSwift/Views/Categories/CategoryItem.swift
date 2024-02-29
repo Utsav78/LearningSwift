ff35f59362ef647d8bb8a414cdf42bf1a5219632
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-02-29 23:37:38 +1030

    Create Category Row

---
+++ LearningSwift/Views/Categories/CategoryItem.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryItem.swift
+//  LearningSwift
+//
+//  Created by Utsav Budathoki on 29/2/2024.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
