2ed82cd5ac5690c0a128641a0b32ad08b76dcbc9
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-03-03 23:25:26 +1030

    feat: Add user profile

---
+++ LearningSwift/Views/Profiles /ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  LearningSwift
+//
+//  Created by Utsav Budathoki on 3/3/2024.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
