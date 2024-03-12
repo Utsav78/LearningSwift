bd98dfe502b22ddf14b3a8341cbfa65631cdaaf3
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-03-12 21:49:41 +1030

    Setup watchOS target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Utsav Budathoki on 12/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
